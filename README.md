3ae8ec1c598c19a603f9065969891c3f8f1022a2
a.liz <amanda.monroy4@gmail.com>
2023-06-05 18:25:01 -0700

    Added the necessary files and code inside to generate the logo.svg file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SVG-generator
+
+## Description
+
+My motivation to build this is because I wanted to create code that can generate either a Trianlge, Square or Circle with text inside the shape, and have the user chose the text color and shape color to allow which will generate the SVG file the chosen variables. 
+
+## Installation
+
+You must install node to run inquirer for the prompt to actually pop up in your temrinal. And install jest.
+
+## Usage
+
+<!-- add video here -->
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
---
+++ index.js
@@ -0,0 +1,72 @@
+const inquirer = require('inquirer');
+const { writeFile } = require('fs/promises');
+const { Square, Triangle, Circle } = require('./lib/shapes.js');
+const SVG = require('./lib/svg.js');
+const Text = require('./lib/text-svg.js')
+
+class CLI {
+    // constructor(content, textColor, shape, shapeColor) {
+    //     this.content = content;
+    //     this.textColor = textColor;
+    //     this.shape = shape;
+    //     this.shapeColor = shapeColor;
+    // }
+    run() {
+        return inquirer 
+            .prompt([
+                {
+                    type: "input",
+                    name: "answer1",
+                    message: "Please enter up to three letters",
+                },
+                {
+                    type: "input",
+                    name: "textColor",
+                    message: "Please enter a color for the text",
+                },
+                {
+                    type: "list",
+                    name: "shape",
+                    message: "Chose a shape",
+                    choices: [
+                        "Square",
+                        "Triangle",
+                        "Circle",
+                    ],
+                },
+                {
+                    type: "input",
+                    name: "shapeColor",
+                    message: "Enter a color for the shape"
+                }
+            ]) .then(({answer1, textColor, shape, shapeColor}) => {
+                let shapeType;
+                switch (shape) {
+                    case "Square":
+                        shapeType = new Square(shapeColor);
+                        break;
+                    case "Triangle":
+                        shapeType = new Triangle(shapeColor);
+                        break;
+                    
+                    case "Circle":
+                        shapeType = new Circle(shapeColor);
+                }
+                let letters = new Text(answer1.toUpperCase(), textColor, shape);
+                let svg = new SVG(shapeType, letters);
+                return writeFile("logo.svg", svg.render());
+            })
+            .then(() => {
+                console.log("Generated logo.svg")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+}
+
+const cli = new CLI();
+
+cli.run();
+
+module.exports = CLI
